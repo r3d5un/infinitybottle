eb74f3067d4390a4ac7f8135faca7d4800759c96
r3d5un <r3d5un@pm.me>
2023-08-06 22:58:11 +0200

    Handle preflight CORS requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -86,6 +86,13 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
+		if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+			w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
